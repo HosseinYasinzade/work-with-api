4c37b1a68ccf5e7391bf62d3fae787153687c2d0
Hossein <hosseinyasinzadeh2002@gmail.com>
2024-11-28 20:29:37 +0330

    fix img sizes & add loading & debug error text

--- src/App.js
+++ src/App.js
@@ -3,30 +3,34 @@ import Lists from "./Lists";
 
 function App() {
   const [images, setImages] = useState([]);
-  const [errors, setError] = useState([]);
+  const [errors, setError] = useState(null);
 
   useEffect(() => {
     const url = "https://restcountries.com/v2/all";
 
-    try {
-      const fetchData = async () => {
+    const fetchData = async () => {
+      try {
         const response = await fetch(url);
         if (!response.ok) {
           throw new Error("field to fetch Please Reload");
         }
         const result = await response.json();
-        console.log(result);
-
         setImages(result);
-      };
-      fetchData();
-    } catch (error) {
-      setError(error);
-    }
+      } catch (error) {
+        setError(error);
+      }
+    };
+    fetchData();
   }, []);
   return (
     <div className="App">
-      {!errors ? <p>errors.message</p> : <Lists images={images} />}
+      {errors ? (
+        <p>{errors.message}</p>
+      ) : images.length > 0 ? (
+        <Lists images={images} />
+      ) : (
+        <p className="loading">Loading...</p>
+      )}
     </div>
   );
 }
